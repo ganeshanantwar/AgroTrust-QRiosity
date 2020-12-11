fa61ac75bd6c7680259b29aa0e9b8f2238d24c87
Ganesh <ganesh@emertech.io>
2020-12-11 17:39:25 +0530

    Know Your Harvest

--- controllers/farmerController.js
+++ controllers/farmerController.js
@@ -1,13 +1,68 @@
+const streamer = require('../utils/streamer');
+
 exports.knowYourConsumer = async (req, res) => {
+	let originid = req.params.originid;
+	let cropid = req.params.cropid;
+	//get origin object
+	let originObject = await streamer.fetchOne('origin', originid, 'GFPCL', [
+		cropid,
+	]);
+
 	res.status(200).send('DEVELOPMENT IN PROGRESS');
 };
 
 exports.knowYourHarvest = async (req, res) => {
-	res.status(200).send('DEVELOPMENT IN PROGRESS');
+	let originid = req.params.originid;
+	let cropid = req.params.cropid;
+	//get origin object
+	let originObject = await streamer.fetchOne('origin', originid, 'GFPCL', [
+		cropid,
+	]);
+
+	//find all BTUs with this origin
+	let btuObjects = await streamer.fetchAll('btu', 'GFPCL', [cropid]);
+	btuObjects = btuObjects.filter(
+		(i) => i.oneDown == null && i.origin == originObject.originCode
+	);
+
+	btuObjects = btuObjects.map((i) => {
+		return {
+			harvestID: i.btuCode,
+			productionCost: i.productionCost,
+			acceptedProduction: i.totalWeight,
+			totalValue: i.totalValue,
+			unitValue: i.unitValue,
+		};
+	});
+
+	return res.status(200).json(btuObjects);
 };
 
 exports.knowYourChannel = async (req, res) => {
-	res.status(200).send('DEVELOPMENT IN PROGRESS');
+	let originid = req.params.originid;
+	let cropid = req.params.cropid;
+	//get origin object
+	let originObject = await streamer.fetchOne('origin', originid, 'GFPCL', [
+		cropid,
+	]);
+
+	//find all BTUs with this origin
+	let btuObjects = await streamer.fetchAll('btu', 'GFPCL', [cropid]);
+	btuInwardObjects = btuObjects.filter(
+		(i) => i.oneDown == null && i.origin == originObject.originCode
+	);
+
+	btuInwardObjects = btuInwardObjects.map((i) => {
+		return {
+			harvestID: i.btuCode,
+			fromLoc: i.transfers[0].fromLoc,
+			toLoc: i.transfers[0].toLoc,
+			date: i.transfers[0].date,
+			totalWeight: i.totalWeight,
+		};
+	});
+
+	return res.status(200).json(btuInwardObjects);
 };
 
 exports.knowYourMarket = async (req, res) => {
--- routes/farmer.js
+++ routes/farmer.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const master = require('../controllers/farmerController');
 
-router.get('/kyconsumer/:originid', master.knowYourConsumer);
-router.get('/kyharvest/:originid', master.knowYourHarvest);
-router.get('/kychannel/:originid', master.knowYourChannel);
-router.get('/kymarket/:originid', master.knowYourMarket);
+router.get('/kyconsumer/:cropid/:originid', master.knowYourConsumer);
+router.get('/kyharvest/:cropid/:originid', master.knowYourHarvest);
+router.get('/kychannel/:cropid/:originid', master.knowYourChannel);
+router.get('/kymarket/:cropid/:originid', master.knowYourMarket);
 
 module.exports = router;
